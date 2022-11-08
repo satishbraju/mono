ea924626c9ebceac0d993e556c12c10e16686102
Satish Raju <satishbraju@gmail.com>
2022-11-08 13:33:20 +0100

    fix workflow

--- exp_c/code/empDetails.py
+++ exp_c/code/empDetails.py
@@ -1,10 +1,9 @@
 class Person:
-  def __init__(self, name, age, loc):
+  def __init__(self, name, age):
     self.name = name
     self.age = age
-    self.loc = loc
-
-p1 = Person("John", 36, Bee)
+    
+p1 = Person("John", 36)
 
 print(p1.name)
 print(p1.age)
