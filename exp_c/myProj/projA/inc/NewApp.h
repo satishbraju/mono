c136ead6b6f07a53ec34b9b91d43f9b048b4b5cc
Satish Raju <satishbraju@gmail.com>
2022-11-16 14:50:29 +0100

    adding NewApp

---
+++ exp_c/myProj/projA/inc/NewApp.h
@@ -0,0 +1,22 @@
+// NewApp.h
+#ifndef NewApp.h
+#define NewApp.h
+
+#include <string>
+
+
+class Stringer{
+
+	public:
+		Stringer();
+		~Stringer();
+		st
+			ring getHelloWorld();
+		void setHellowWorld(string);
+		
+	private:
+		string helloWorld;
+
+}
+
+#endif //NewApp.h
