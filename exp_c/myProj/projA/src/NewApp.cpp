32baf43b534d99ea5c15520cfcdc0e612dc6e641
Satish Raju <satishbraju@gmail.com>
2022-11-16 14:51:10 +0100

    Adding NewApp

---
+++ exp_c/myProj/projA/src/NewApp.cpp
@@ -0,0 +1,61 @@
+/* NewApp.cpp
+   This is a hello world
+*/
+
+#include "NewApp.h"
+#include <stdio.h>
+#include <string.h>
+
+
+Stringer::Stringer(){
+// Initializes class
+//
+// input: none
+// output:none
+//
+
+	helloWorld = "Hello World";
+
+}
+
+Stringer::setHelloWorld(string inVal){
+// Sets attribute hello world 
+//
+// input: string value
+// output:none
+//
+
+	helloWorld = inVal;
+
+}
+
+Stringer::getHelloWorld(){
+// Returns hello world
+//
+// input: none
+// output: string value
+//
+
+	return helloWorld;
+
+}
+
+// MAIN FUNCTION
+
+using namespace std;
+
+int main(int argc, chard ** argv){
+
+	Stringer hello;
+	string writeOut;
+
+	hello.setHelloWorld("Hello World!");
+
+	writeOut = hello.getHelloWorld();
+
+	cout << "Here is my string: " << endl;
+	cout  << writeOut << endl;
+
+	exit(0);
+
+}
